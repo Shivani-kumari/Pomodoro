2529f6260854544d1e6d09355621b7ad6ea4bbb3
shivani026kumari <shivanikumari5@yahoo.com>
2021-06-11 13:52:30 +0530

    added check  box

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,10 +3,19 @@ import {TaskListContext} from '../context/TaskListContext'
 
 function Task({task}) {
     const {removeTask,findItem} = useContext(TaskListContext)
+    const [checked, setChecked] = React.useState(false);
     return (
         <div>
             <li className="list-item">
-                <span>{task.title}</span>
+            <label>
+            <input type="checkbox"
+            defaultChecked={checked}
+            onChange={() => setChecked(!checked)}
+             />
+                {checked === false && <span>{task.title}</span>}
+                {checked === true && <span style={{textDecorationLine:"line-through"}}>{task.title}</span>}
+            </label>
+                
                 <button className="btn-delete task-btn" onClick={()=>removeTask(task.id)}>
                 <i className="fas fa-trash-alt"></i>
                 </button>
